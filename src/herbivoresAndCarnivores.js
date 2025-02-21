048af4624673b575745531091f00cda0376e0071
Ivan Herhi <ionherhi@gmail.com>
2025-02-21 12:15:36 +0100

    CarnevoreANDerbivore

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,49 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  health = 100;
+  name;
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+
+  constructor(name) {
+    super();
+    this.name = name;
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super();
+    this.name = name;
+    Animal.alive.push(this);
+  }
+
+  bite(animal) {
+    if (animal instanceof Carnivore) {
+      return;
+    }
+
+    if (animal.hidden !== true && animal.health >= 50) {
+      animal.health = animal.health - 50;
+    }
+
+    if (animal.health === 0) {
+      const indexThis = Animal.alive.indexOf(animal);
+
+      if (indexThis !== -1) {
+        Animal.alive.splice(indexThis, 1);
+      }
+    }
+  }
 }
 
 module.exports = {
